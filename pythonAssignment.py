8ab2412d7d6fb450d4134d9f0e522a29bcdd6dd7
Ogundimu Royan <111058308+Skimzyy@users.noreply.github.com>
2023-03-25 21:48:07 +0100

    Ogundimu Royan 21/0151

---
+++ pythonAssignment.py
@@ -0,0 +1,23 @@
+def sum():
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    print("The sum of {} + {} = {}".format(a, b, a + b))
+def difference():
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    print("The difference of {} - {} = {}".format(a, b, a - b))
+def product():
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    print("The product of {} * {} = {}".format(a, b, a * b))
+def division():
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    print("The division of {} / {} = {}".format(a, b, a / b))
+def modulo():
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    print("The modulo of {} % {} = {}".format(a, b, a % b))
+
+modulo()
+sum()
